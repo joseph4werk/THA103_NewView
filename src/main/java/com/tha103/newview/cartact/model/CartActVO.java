29897d2bbad228337e94482201a73a06d040a915
Joseph Sun <joseph4werk@gmail.com>
2023-10-07 13:39:42 +0800

    移除 CartActVO 中建構子、get/setter、toString、欄位變數 Association 後多餘物件

--- src/main/java/com/tha103/newview/cartact/model/CartActVO.java
+++ src/main/java/com/tha103/newview/cartact/model/CartActVO.java
@@ -23,14 +23,6 @@ public class CartActVO implements java.io.Serializable {
 	private Integer cartActID;
 
 	@Expose
-	@Column(name = "userID")
-	private Integer userID;
-
-	@Expose
-	@Column(name = "actID")
-	private Integer actID;
-
-	@Expose
 	@Column(name = "cartQuantity")
 	private Integer cartQuantity;
 
@@ -47,12 +39,9 @@ public class CartActVO implements java.io.Serializable {
 		// TODO Auto-generated constructor stub
 	}
 
-	public CartActVO(Integer cartActID, Integer userID, Integer actID, Integer cartQuantity, UserVO userVO,
-			ActVO actVO) {
+	public CartActVO(Integer cartActID, Integer cartQuantity, UserVO userVO, ActVO actVO) {
 		super();
 		this.cartActID = cartActID;
-		this.userID = userID;
-		this.actID = actID;
 		this.cartQuantity = cartQuantity;
 		this.userVO = userVO;
 		this.actVO = actVO;
@@ -64,22 +53,6 @@ public class CartActVO implements java.io.Serializable {
 
 	public void setCartActID(Integer cartActID) {
 		this.cartActID = cartActID;
-	}
-
-	public Integer getUserID() {
-		return userID;
-	}
-
-	public void setUserID(Integer userID) {
-		this.userID = userID;
-	}
-
-	public Integer getActID() {
-		return actID;
-	}
-
-	public void setActID(Integer actID) {
-		this.actID = actID;
 	}
 
 	public Integer getCartQuantity() {
@@ -108,8 +81,10 @@ public class CartActVO implements java.io.Serializable {
 
 	@Override
 	public String toString() {
-		return "CartActVO [cartActID=" + cartActID + ", userID=" + userID + ", actID=" + actID + ", cartQuantity="
-				+ cartQuantity + ", userVO=" + userVO + ", actVO=" + actVO + "]";
+		return "CartActVO [cartActID=" + cartActID + ", cartQuantity=" + cartQuantity + ", userVO=" + userVO
+				+ ", actVO=" + actVO + "]";
 	}
+
+	
 
 }
