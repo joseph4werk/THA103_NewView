18fe84d516e1abaf9855e4baf59e07771850ea4f
Zihling <a201010810601@gmail.com>
2023-10-26 12:01:44 +0800

    修改購物車取值方式

--- src/main/java/com/tha103/newview/cartact/controller/GetCartController.java
+++ src/main/java/com/tha103/newview/cartact/controller/GetCartController.java
@@ -1,6 +1,9 @@
 package com.tha103.newview.cartact.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -10,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import com.tha103.newview.act.service.ActService;
 import com.tha103.newview.act.service.ActServiceImpl;
 import com.tha103.newview.actpic.model.ActPic;
@@ -41,74 +45,82 @@ public class GetCartController extends HttpServlet {
 		Jedis jedis = JedisPoolUtil.getJedisPool().getResource();
 		jedis.select(0); // 選擇 Redis db(0)
 
-		// 3-1. 藉由獲取 Redis Hash 裁切資料後得到actID、userID、座位狀態，以及座位行列資訊
-		// 3-2. 其中座位行列要先透過ActVO得到Scope資料，才能知道座位數量，進一步轉換為行列資訊
-
-		// 3-3. 先得到所有Hash資料
+		// 3.藉由獲取RedisHash，裁切資料後得到actID、userID、座位狀態，以及座位行列資訊，其中座位行列要先透過ActVO得到Scope資料，才能知道座位數量，進一步轉換為行列資訊
+		// 3-1. 先得到所有Hash資料
 		Set<String> keys = jedis.keys("*");
 		for (String key : keys) {
 			System.out.println(key);
 
-			// 3-4. 再得到單一Hash資訊
+			// 3-2. 再得到單一Hash資訊
 			Map<String, String> hashData = jedis.hgetAll(key);
 			for (Map.Entry<String, String> entry : hashData.entrySet()) {
 				String SeatDatakey = entry.getKey();
 				String SeatDatavalue = entry.getValue();
-				System.out.println("Key: " + SeatDatakey + ", Value: " + SeatDatavalue);
 
-				// 3-5. 再裁切取值
+				// 3-3. 再裁切取值
 				String[] Seatkey = SeatDatakey.split(",");
 				int seatNumber = Integer.parseInt(Seatkey[0]); // 使用 parseInt 來轉換
 				String[] Seatvalue = SeatDatavalue.split(",");
 				int cartUID = Integer.parseInt(Seatvalue[0]); // 使用 parseInt 來轉換
 				int actID = Integer.parseInt(Seatvalue[1]); // 使用 parseInt 來轉換
 				String seatStatus = Seatvalue[2];
-				System.out.println("座位編號(要轉換成行列): " + seatNumber + "，cartUID: " + cartUID + "，actID: " + actID
-						+ "，seatStatus: " + seatStatus);
 
+				// 4. 開始處理座位資料
 				// 4-1. 先獲取Scope資料，呼叫ActService
 				ActService actService = new ActServiceImpl();
 				int scope = actService.findByPrimaryKey(actID).getActScope();
-				System.out.println("scope: " + scope);
 
 				// 4-2. 使用TurnSeatNumberToRowColumn方法將座位編號轉換座位行列
 				String rowColumn = TurnSeatNumberToRowColumn(seatNumber, scope);
-				System.out.println("轉換後的行列: " + rowColumn);
 
-				// 5. 取得其他購物車資訊(活動名稱、活動單價、廠商ID、廠商名稱
+				// 5. 取得其他購物車資訊(活動名稱、活動單價、廠商ID、廠商名稱等)
+				// 5-1. 基本資料處理
 				Integer pubID = actService.findByPrimaryKey(actID).getPublisherVO().getPubID();
 				String pubName = actService.findByPrimaryKey(actID).getPublisherVO().getPubName();
 				String actName = actService.findByPrimaryKey(actID).getActName();
-				Integer actPrice = actService.findByPrimaryKey(actID).getActPrice();			
-				Integer actCount = Seatkey.length;
-				Integer actPriceTotal = actCount* actPrice;
-				Set<ActPic> actPic = actService.findByPrimaryKey(actID).getActpics();
-				System.out.println("pubID: " + pubID + ","+ "pubName: " + pubName+ "," + "actName: " + actName + "," + "actPrice: " + actPrice + "," +  "actCount: " + actCount + ","+  "actPriceTotal: " + actPriceTotal + ","+  "actPic: " + actPic);
+				Integer actPrice = actService.findByPrimaryKey(actID).getActPrice();
+				Integer actCount = SeatDatakey.length() - 1;
+				Integer actPriceTotal = actCount * actPrice;
+
+				// 5-2. 圖片處理
+				List<byte[]> actPicsList = new ArrayList<>();
+				Set<ActPic> actPics = actService.findByPrimaryKey(actID).getActpics();
+				for (ActPic actPic : actPics) {
+					if (actPic.getActPic() != null) {
+						byte[] actPicData = actPic.getActPic();
+						actPicsList.add(actPicData);
+					}
+				}
+
+				// 5-4. 打印測試
+				System.out.println("pubID: " + pubID + "," + "pubName: " + pubName + "actID: " + actID + "," + "scope: "
+						+ scope + "," + "actPics: " + actPicsList + "," + "actName: " + actName + "," + "rowColumn: "
+						+ rowColumn + "," + "actPrice: " + actPrice + "," + "actCount: " + actCount + ","
+						+ "actPriceTotal: " + actPriceTotal);
 
 				// 6. 回傳Json資料到購物車
 //				if (userID != null && userID.equals(cartUID)) {
-//					Map<String, Object> jsonData = new HashMap<>();
-//					jsonData.put("userID", userID);
-//					jsonData.put("actID", actID);
-//					jsonData.put("rowColumn", rowColumn);
-//					jsonData.put("seatStatus", seatStatus);
-//					jsonData.put("actName", actName);
-//					jsonData.put("actPrice", actPrice);
-//					jsonData.put("pubID", pubID);
-//					jsonData.put("actPic", actPic);
-//
-//					System.out.println(jsonData);
-//
-//					Gson gson = new Gson();
-//					String json = gson.toJson(jsonData);
+				Map<String, Object> jsonData = new HashMap<>();
+				jsonData.put("pubID", pubID);
+				jsonData.put("pubName", pubName);
+				jsonData.put("actID", actID);
+				jsonData.put("scope", scope);
+				jsonData.put("actPics", actPicsList);
+				jsonData.put("actName", actName);
+				jsonData.put("rowColumn", rowColumn);
+				jsonData.put("actPrice", actPrice);
+				jsonData.put("actCount", actCount);
+				jsonData.put("actPriceTotal", actPriceTotal);
+
+				System.out.println(jsonData);
+
+				Gson gson = new Gson();
+				String json = gson.toJson(jsonData);
 //					res.getWriter().write(json);
 //				}
-
 			}
-		}
 
-		// 6. 關閉 Redis
-		jedis.close();
+		}
 
 	}
 
@@ -133,6 +145,6 @@ public class GetCartController extends HttpServlet {
 			break;
 		}
 
-		return "Row: " + rowIndex + ", Column: " + seatIndex;
+		return "第 " + rowIndex + "排" + "," + "第 " + seatIndex + "列";
 	}
 }
