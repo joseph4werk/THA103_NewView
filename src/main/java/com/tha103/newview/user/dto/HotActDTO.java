c9b225b2f2065784c5732e2f365173d009539007
Joseph Sun <joseph4werk@gmail.com>
2023-10-29 13:24:16 +0800

    修正會員中心 DTO 圖片 null 問題 (資料庫仍需在 ActPic 預先建立假資料才符合邏輯)

--- src/main/java/com/tha103/newview/user/dto/HotActDTO.java
+++ src/main/java/com/tha103/newview/user/dto/HotActDTO.java
@@ -9,27 +9,29 @@ import com.tha103.newview.act.model.ActVO;
 import com.tha103.newview.actpic.model.ActPic;
 
 public class HotActDTO {
-	
+
 	List<Integer> actID = new ArrayList<>();
 
 	List<String> actName = new ArrayList<>();
-	
+
 	List<String> actPic = new ArrayList<>();
-	
+
 	public HotActDTO(ActVO actVO) {
-		
+
 		this.actID.add(actVO.getActID());
 		this.actName.add(actVO.getActName());
-		
+
 		Set<ActPic> actpics = actVO.getActpics();
 		Object[] objectArray = actpics.toArray();
 		ActPic[] actPicsArray = new ActPic[objectArray.length];
-		if(objectArray[0] instanceof ActPic) {
+		if (objectArray[0] instanceof ActPic) {
 			actPicsArray[0] = (ActPic) objectArray[0];
-			this.actPic.add(Base64.getEncoder().encodeToString(actPicsArray[0].getActPic()));
+			if (actPicsArray[0] != null) {
+				this.actPic.add(Base64.getEncoder().encodeToString(actPicsArray[0].getActPic()));
+			}
 		}
 	}
-	
+
 	public HotActDTO() {
 		super();
 		// TODO Auto-generated constructor stub
--- src/main/java/com/tha103/newview/user/dto/MyLikeActDTO.java
+++ src/main/java/com/tha103/newview/user/dto/MyLikeActDTO.java
@@ -11,9 +11,9 @@ import com.tha103.newview.mylike.model.MyLikeVO;
 public class MyLikeActDTO {
 
 	List<String> actName = new ArrayList<>();
-	
+
 	List<String> actIntroduce = new ArrayList<>();
-	
+
 	List<String> actPic = new ArrayList<>();
 
 	public MyLikeActDTO(MyLikeVO myLikeVO) {
@@ -21,10 +21,12 @@ public class MyLikeActDTO {
 		this.actName.add(myLikeVO.getAct().getActName());
 		this.actIntroduce.add(myLikeVO.getAct().getActIntroduce());
 		Set<ActPic> actPics = myLikeVO.getAct().getActpics();
-		
+
 		for (ActPic actPic : actPics) {
 			byte[] actPicByte = actPic.getActPic();
-			this.actPic.add(Base64.getEncoder().encodeToString(actPicByte));
+			if (actPicByte != null) {
+				this.actPic.add(Base64.getEncoder().encodeToString(actPicByte));
+			}
 		}
 	}
 
