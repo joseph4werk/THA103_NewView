3e01c72b92ac04bbbbd1f35bac55ef1ddeeb8d12
Mandy Jain <nicky8221s@gmail.com>
2023-10-29 16:16:06 +0800

    暫存檔案

--- src/main/java/com/tha103/newview/orders/service/OrdersServiceImpl.java
+++ src/main/java/com/tha103/newview/orders/service/OrdersServiceImpl.java
@@ -1,11 +1,17 @@
 package com.tha103.newview.orders.service;
 
+import java.sql.Timestamp;
 import java.util.List;
 
+import org.hibernate.Session;
+
+import com.tha103.newview.discount.model.DiscountVO;
 import com.tha103.newview.orders.model.OrdersDAO;
 import com.tha103.newview.orders.model.OrdersDAOImpl;
 import com.tha103.newview.orders.model.OrdersVO;
-
+import com.tha103.newview.publisher.model.PublisherVO;
+import com.tha103.newview.user.model.UserVO;
+import com.tha103.util.HibernateUtil;
 
 public class OrdersServiceImpl implements OrdersService {
 
@@ -38,21 +44,87 @@ public class OrdersServiceImpl implements OrdersService {
 		return ordersDao.getAll();
 	}
 
-
 	@Override
 	public int deleteOrdersbyUserIDandPubID(Integer userID, Integer pubID) {
 		// TODO Auto-generated method stub
-		return ordersDao.deleteOrdersbyUserIDandPubID(userID,pubID);
+		return ordersDao.deleteOrdersbyUserIDandPubID(userID, pubID);
 	}
 
 	@Override
 	public Integer getOrderBy(int userID, int pubID) {
 		// TODO Auto-generated method stub
-		return ordersDao.getOrderBy(userID,pubID);
+		return ordersDao.getOrderBy(userID, pubID);
 	}
 
-	
+	public OrdersVO insertOrders(Integer userID, Integer ordTotal, Integer discount, Integer discountPrice,
+			Timestamp ordTime, Integer pubID, Integer ordType, Integer actQuantity, Integer discountNO) {
 
-	
+		OrdersVO ordersVO = new OrdersVO();
+
+		UserVO userVO = new UserVO();
+		userVO.setUserID(userID);
+		ordersVO.setUserVO(userVO);
+
+		ordersVO.setOrdTotal(ordTotal);
+		ordersVO.setDiscount(discount);
+		ordersVO.setDiscountPrice(discountPrice);
+		ordersVO.setOrdTime(ordTime);
+
+		PublisherVO publisherVO = new PublisherVO();
+		publisherVO.setPubID(pubID);
+		ordersVO.setPublisherVO(publisherVO);
+
+		ordersVO.setOrdType(ordType);
+		ordersVO.setActQuantity(actQuantity);
+
+		DiscountVO discountVO = new DiscountVO();
+		discountVO.setDiscountNO(discountNO);
+		ordersVO.setDiscountVO(discountVO);
+
+		return ordersVO;
+	}
+
+	public OrdersVO updateOrders(Integer userID, Integer ordTotal, Integer discount, Integer discountPrice,
+			Timestamp ordTime, Integer pubID, Integer ordType, Integer actQuantity, Integer discountNO) {
+
+		OrdersVO ordersVO = new OrdersVO();
+
+		UserVO userVO = new UserVO();
+		userVO.setUserID(userID);
+		ordersVO.setUserVO(userVO);
+
+		ordersVO.setOrdTotal(ordTotal);
+		ordersVO.setDiscount(discount);
+		ordersVO.setDiscountPrice(discountPrice);
+		ordersVO.setOrdTime(ordTime);
+
+		PublisherVO publisherVO = new PublisherVO();
+		publisherVO.setPubID(pubID);
+		ordersVO.setPublisherVO(publisherVO);
+
+		ordersVO.setOrdType(ordType);
+		ordersVO.setActQuantity(actQuantity);
+
+		DiscountVO discountVO = new DiscountVO();
+		discountVO.setDiscountNO(discountNO);
+		ordersVO.setDiscountVO(discountVO);
+
+		return ordersVO;
+
+	}
+
+	public List<UserVO> getAllUsers() {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		try {
+			session.beginTransaction();
+			List<UserVO> users = session.createQuery("from UserVO", UserVO.class).list();
+			session.getTransaction().commit();
+			return users;
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return null;
+		}
+	}
 
 }
